695809b186a020f0d17a79fae881ffb08cbcfa9b
kovarnyirus <kovarnyi@gmail.com>
2019-03-26 20:55:08 +0300

    sliders

--- source/js/main.js
+++ source/js/main.js
@@ -20,8 +20,8 @@ const mainNav = document.querySelector('.main-nav');
 
 //аккордион блока програм
 for (let i = 0; i < programAccordions.length; i++) {
-  programAccordions[i].addEventListener("click", function(event) {
-    if(event.target.className === 'program-toggle'){
+  programAccordions[i].addEventListener("click", function (event) {
+    if (event.target.className === 'program-toggle') {
       this.classList.toggle("active");
     }
 
@@ -31,9 +31,9 @@ for (let i = 0; i < programAccordions.length; i++) {
 // аккардион блока ответов
 
 for (let i = 0; i < answersAccordions.length; i++) {
-  answersAccordions[i].addEventListener("click", function(event) {
+  answersAccordions[i].addEventListener("click", function (event) {
     console.log(event.target.className);
-    if((event.target.className === 'accordion__item-title') || (event.target.className.animVal === 'accordion__arrow')){
+    if ((event.target.className === 'accordion__item-title') || (event.target.className.animVal === 'accordion__arrow')) {
       this.classList.toggle("active");
     }
   });
@@ -41,7 +41,7 @@ for (let i = 0; i < answersAccordions.length; i++) {
 
 // главное меню
 
-function onMouseDownNavToggle(){
+function onMouseDownNavToggle() {
   if (mainNav.classList.contains("main-nav--closed")) {
     mainNav.classList.remove("main-nav--closed");
     mainNav.classList.add("main-nav--opened");
@@ -54,3 +54,60 @@ function onMouseDownNavToggle(){
 }
 
 mainNavToggle.addEventListener('mousedown', onMouseDownNavToggle);
+
+
+$('.photo-gallery__list').slick({
+  infinite: true,
+  slidesToShow: 4,
+  arrows: true,
+  appendArrows: $('.photo-gallery__arrows'),
+  prevArrow: $('.arrow__left'),
+  nextArrow: $('.arrow__right'),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+
+});
+
+$('.certificate__gallery').slick({
+  infinite: true,
+  slidesToShow: 4,
+  arrows: true,
+  appendArrows: $('.certificate__arrows'),
+  prevArrow: $('.certificate__arrow-left'),
+  nextArrow: $('.certificate__arrow-right'),
+  centerPadding: '1px',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 1140,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+  ]
+
+});
+
+
+$('.comment__list').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  leftMode: true,
+  variableWidth: true,
+  appendArrows: $('.comment__arrows'),
+  prevArrow: $('.arrow__left'),
+  nextArrow: $('.arrow__right'),
+
+});
